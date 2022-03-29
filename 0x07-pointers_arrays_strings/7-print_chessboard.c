0ec3f5478a19a4583fb76fe473424c350910e14c
Etypo <64205629+Etypo@users.noreply.github.com>
2022-03-29 12:39:00 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
-  *print_chessboard - prints the chessboard.
-  *@a: array of pointers.
-  *
-  *Return: void.
-  */
+ * print_chessboard - prints a chess board
+ * @a: the chess board
+ *
+ * Return: void
+ */
 void print_chessboard(char (*a)[8])
 {
-	int  i, j;
+	int i;
+	int j;
 
-	for (i = 0; i <= 7; i++)
+	for (j = 0; j < 8; j++)
 	{
-		for (j = 0; j <= 7; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar(10);
+		for (i = 0; i < 8; i++)
+			_putchar(a[j][i]);
+		_putchar('\n');
 	}
 }
