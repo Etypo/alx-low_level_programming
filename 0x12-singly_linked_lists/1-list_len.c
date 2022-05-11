bd4cb2db40ca5df27e3f4a6b02c78a88968f16d1
Etypo <64205629+Etypo@users.noreply.github.com>
2022-05-11 13:49:02 -0700

    Task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - function that returns the number of elements in a linked
+ * list_t list
+ * @h: header pointer
+ * Return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+	size_t cont = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		h = h->next;
-		nelem++;
+		cont++;
 	}
-	return (nelem);
+	return (cont);
 }
